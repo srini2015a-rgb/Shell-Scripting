b8a58da29fe71084770f249d27c35531eb09af5f
Sreenivasulu P <srini2015a@gmail.com>
2026-05-22 05:37:28 +0100

    Shell Scripting

---
+++ 04-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+User1=$1
+User2=
+
+echo "$User1 :: Hi, Are you alright?"
+echo "$User2 :: I am doing great $User1, how about you $User2?"
+echo "$User1 :: Well. Thanks for asking. I have been focuing on AI and ML these days."
+echo "$User2 :: Oh, that is great. I am also learning AI and ML these days. I am really enjoying it.!!"
