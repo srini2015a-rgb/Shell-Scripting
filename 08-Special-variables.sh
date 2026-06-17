7a23f68168eb6d205385c691588a1761edf9d02f
Sreenivasulu P <srini2015a@gmail.com>
2026-06-17 02:27:25 +0100

    Shell Scripting

--- 08-Special-variables.sh
+++ 08-Special-variables.sh
@@ -6,4 +6,4 @@ echo " Script name that is beinng executed : $0"
 echo "Current directory is : $PWD"
 echo "User executing the script is : $USER"
 echo "User home directory is : $HOME"
-
+echo "PID of the script is : $$" #Process instance ID of the script
