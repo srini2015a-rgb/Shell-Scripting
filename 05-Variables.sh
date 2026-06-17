f29cd9a700ac6dc14298dbfd15480cd4f7839f5f
Sreenivasulu P <srini2015a@gmail.com>
2026-06-17 01:48:23 +0100

    Shell Scripting

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -3,4 +3,6 @@ user1=$1
 echo "Please enter the pin number"
 #read PIN_Number # PIN_Number is a variable which will store the value entered by the user
 read -s PIN_Number # -s option is used to hide the input entered by the user
+read -s PASSWORD
 echo "Pin number entered by the $user1 is :: $PIN_Number"
+
