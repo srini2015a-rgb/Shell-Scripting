a50b14e0b6c79eeb81e1b252f277b93040b12279
Sreenivasulu P <srini2015a@gmail.com>
2026-06-17 02:26:25 +0100

    Shell Scripting

--- 08-Special-variables.sh
+++ 08-Special-variables.sh
@@ -3,4 +3,7 @@
 echo "All Variables passed to the script : $@"
 echo "All Variables passed to the script : $*"
 echo " Script name that is beinng executed : $0"
+echo "Current directory is : $PWD"
+echo "User executing the script is : $USER"
+echo "User home directory is : $HOME"
 
