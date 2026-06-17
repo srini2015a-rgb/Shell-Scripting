d56ad4d7c17456ac0a4d9574c61d4fc7dc5cfd5f
Sreenivasulu P <srini2015a@gmail.com>
2026-06-17 02:17:43 +0100

    Shell Scripting

--- 07-Variables.sh
+++ 07-Variables.sh
@@ -3,7 +3,8 @@
 #DATE=$(date)
 START_TIME=$(date +%s)
 
-Sleep 10 &
+Sleep 10 
+
 END_TIME=$(date +%s)
-TOTAL_TIME=$((END_TIME - START_TIME))
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "Execution time in seconds is: $TOTAL_TIME"
