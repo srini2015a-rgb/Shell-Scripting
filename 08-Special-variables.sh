51aa4b6ef9af8b56010bd17706fc2c3900b50dc1
Sreenivasulu P <srini2015a@gmail.com>
2026-06-17 02:28:26 +0100

    Shell Scripting

--- 08-Special-variables.sh
+++ 08-Special-variables.sh
@@ -7,3 +7,7 @@ echo "Current directory is : $PWD"
 echo "User executing the script is : $USER"
 echo "User home directory is : $HOME"
 echo "PID of the script is : $$" #Process instance ID of the script
+
+sleep 60 &
+
+echo "PID of the last background process is : $!" #Process instance ID of the last background process
