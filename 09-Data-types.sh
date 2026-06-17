12d22df983f9d3af9b4a9d696717a2f6626c2b4b
Sreenivasulu P <srini2015a@gmail.com>
2026-06-17 02:47:41 +0100

    Shell Scripting

--- 09-Data-types.sh
+++ 09-Data-types.sh
@@ -8,6 +8,12 @@ NUMBER2=200
 NAME=Srini
 SUM=$((NUMBER1+NUMBER2+${NAME}))
 echo "Sum is $SUM"
-FRIENDS="Jack   Jill   John"
-echo "Friends are $FRIENDS"
+#FRIENDS="Jack   Jill   John"
+
+#echo "Friends are $FRIENDS"
+
+FRIENDS=("Jack" "Jill" "John")
+echo "All Friends are ${FRIENDS[@]}"
+echo "Second Friend is ${FRIENDS[1]}"
+
 
